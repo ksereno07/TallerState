bc62b18957bb799a9a492a0e3d07771ae99e5b67
keiner sereno <ksereno@unab.edu.co>
2026-09-23 15:06:33 -0500

    maqueta

---
+++ app/src/main/java/me/keinersereno/statesjatpackcompose/Statescreen.kt
@@ -0,0 +1,31 @@
+package me.keinersereno.statesjatpackcompose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showSystemUi = true)
+@Composable
+fun Statescreen () {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("contador: 0", fontSize = 32.sp)
+        Spacer((Modifier.height(25.dp)))
+        Button(onClick = {}) {
+            Text("sumar 1")
+        }
+    }
+}
